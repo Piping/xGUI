c8a1d60df76664fcc5f2a2061e3e9b06f1938c3f
Robin <dequanzh@usc.edu>
2020-04-11 10:44:45 -0700

    update Makefile to make.sh

--- make.sh
+++ make.sh
@@ -1,36 +1,50 @@
-bootstrap:
+bootstrap() {
 	sudo apt install -y curl git
 	sudo apt install -y build-essential libx11-dev xorg-dev
 	sudo apt install -y sxhkd i3lock rofi xwallpaper
+}
 
-dwn:
+dwn() {
 	cd dwm/ && sudo make install
 	mkdir -p ~/.config/dwm
 	cp -r config/dwm ~/.config/
+}
 
-sxhkd:
+sxhkd() {
 	mkdir -p ~/.config/sxhkd/
 	cp -r config/sxhkd ~/.config/
+}
 
-rofi:
+rofi() {
 	mkdir -p ~/.config/rofi/
 	cp -r config/rofi/ ~/.config/
+}
 
-install: dwn sxhkd rofi
+install() {
+    dwn
+    sxhkd
+    rofi
+}
 
-sync:
+sync() {
 	# cp -r ~/.confid/rofi config/
 	cp -r ~/.config/sxhkd config/
 	cp -r ~/.config/dwm config/
+}
 
-kvm-bootstrap:
+kvm-bootstrap() {
 	sudo apt install qemu qemu-system virt-manager ebtables
 	sudo systemctl enable --now libvirtd
 	sudo usermod -G libvirtd -a robin
+}
 
-kvm-convert:
+kvm-convert() {
 	sudo qemu-img convert -f vid -O "${DISK}" "/var/lib/libvirt/images/${DISK}.qcow2"
+}
 
-font: #patched fonts
+font() {
 	git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 	cd nerd-fonts && chmod +x install.sh && sudo ./install.sh -S
+}
+
+"$@"
