94873d7852e336102afc6a3860490f28b7a3e8f0
Robin <dequanzh@usc.edu>
2020-05-15 21:56:28 -0700

    update alacritty terminal setting

--- dwm/config.h
+++ dwm/config.h
@@ -71,12 +71,18 @@ static const Layout layouts[] = {
 	{ NULL,      NULL },
 };
 
+/* commands */
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
+
 /* key definitions */
 #define MODKEY Mod4Mask
 static Key keys[] = {
 	/* modifier             key        function        argument */
 	{ MODKEY,               XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,     XK_r,      quit,           {1} }, 
+	{ MODKEY,               XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,     XK_Return, spawn,          {.v = browsercmd } },
 	{ MODKEY,               XK_b,      togglebar,      {0} },
 	/* Switch to specific layouts */
 	/* { MODKEY,               XK_t,      setlayout,      {.v = &layouts[0]} }, */
